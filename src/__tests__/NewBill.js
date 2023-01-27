72a921a52f1428648e2fd4206cf2ca9fe6181261
Lucie Caillat <lucie.caillat@gmail.com>
2023-01-27 10:51:44 +0100

    refactoring

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -6,83 +6,75 @@ import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import router from '../app/Router';
+import { ROUTES, ROUTES_PATH } from '../constants/routes';
 
 
-describe("Given I am connected as an employee", () => {
+describe("Unit test", () => {
 
-  describe("When I am on NewBill Page", () => {
+  describe("When the function handleChangeFile() of class NewBill is used", () => {
 
     describe("when I upload a file with the wrong format", () => {
 
-      test("then the function handleChangeFile should return an error message ", () => {
-        const objInstance = new NewBill(
-          {
-            document: {
-              querySelector: (s) => {
-                if (s === 'input[data-testid="file"]') {
-                  return {
-                    files: [
-                      new File(["hello"], "hello.txt", { type: "document/txt" }),
-                    ],
-                    addEventListener: () => true,
-                  };
-                } else {
-                  return { addEventListener: () => true };
-                }
-              },
-            },
-
+      test("then it should return an error message ", () => {
+        const wrongFormatFile = new File(["hello"], "hello.txt", { type: "document/txt" })
+        const documentMock = {
+          querySelector: (s) => {
+            if (s === 'input[data-testid="file"]') {
+              return {
+                files: [wrongFormatFile],
+                addEventListener: () => true,
+              }
+            } else {
+              return { addEventListener: () => true }
+            }
+          },
+        }        
+        const objInstance = new NewBill({
+          document: documentMock,
           onNavigate: {},
-
-          store: {},
-            
-          localStorage
-          }
-        );
-        const mockAlert = jest.fn();
-        global.alert = mockAlert;
-        objInstance.handleChangeFile({ preventDefault: () => true });
-        expect(mockAlert.mock.calls).toHaveLength(1);
+          store: {},            
+          localStorage: {}
+        });
+        const mockAlert = jest.fn()
+        global.alert = mockAlert
+        objInstance.handleChangeFile({ preventDefault: () => true })
+        expect(mockAlert.mock.calls).toHaveLength(1)
       })
     });
 
     describe("when I upload a file with the good format", () => {
 
-      test("then the function handleChangeFile should save the user's email", () => {
+      test("then it should save the user's email", () => {
 
         localStorage.setItem("user", '{"email" : "user@email.com"}')
         const createMock = jest.fn()
-
-        const objInstance = new NewBill(
-          {
-            document: {
-              querySelector: (s) => {
-                if (s === 'input[data-testid="file"]') {
-                  return {
-                    files: [
-                      new File(['img'], 'image.png', { type: 'image/png' }),
-                    ],
-                    addEventListener: () => true,
-                  };
-                } else {
-                  return { addEventListener: () => true };
-                }
-              },
-            },
-
-            onNavigate: {},
-
-            store: {
-              bills: () => {
-                return {
-                  create: createMock.mockResolvedValue({fileUrl: "fileURL", key: "key"}) 
-                }
-              }
-            },
-
-            localStorage
+        const goodFormatFile = new File(['img'], 'image.png', { type: 'image/png' })
+        const documentMock ={
+          querySelector: (s) => {
+            if (s === 'input[data-testid="file"]') {
+              return {
+                files: [goodFormatFile],
+                addEventListener: () => true,
+              };
+            } else {
+              return { addEventListener: () => true }
+            }
           },
-          );
+        }
+        const storeMock ={
+          bills: () => {
+            return {
+              create: createMock.mockResolvedValue({fileUrl: "fileURL", key: "key"}) 
+            }
+          }
+        }
+        const objInstance = new NewBill({
+          document: documentMock,
+          onNavigate: {},
+          store: storeMock,
+          localStorage: {}
+        });
 
         objInstance.handleChangeFile({ 
           preventDefault: () => true ,
@@ -90,6 +82,6 @@ describe("Given I am connected as an employee", () => {
         })
         expect(createMock.mock.calls[0][0].data.get("email")).toEqual("user@email.com")
       })
-    });   
+    })   
   })
 })
